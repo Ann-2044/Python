563b3c571514bca222d6303790636b30dad31c65
Ann-2044 <24mp2293@rit.ac.in>
2024-12-07 14:52:22 +0530

    exp2.py

---
+++ lab_cycle_1/exp2.py
@@ -0,0 +1,6 @@
+num1=10
+num2=10.5
+complex_num=2+3j
+print(f"number:{num1} has type:{type(num1)}")
+print(f"number:{num1} has type:{type(num2)}")
+print(f"complex:{complex_num} has type:{type(complex_num)}") 
