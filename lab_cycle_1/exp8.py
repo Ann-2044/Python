b599d02624f02caecd6240d2656941a9fa6244f9
Ann-2044 <24mp2293@rit.ac.in>
2024-12-07 15:00:41 +0530

    Create exp8.py

---
+++ lab_cycle_1/exp8.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+if a>b and a>c:
+        print(f"{a} is greater")
+elif b>a and b>c:
+        print(f"{b} is greater")
+else:
+	print(f"{c} is greater")
